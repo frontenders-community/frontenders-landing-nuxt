1af17fe8b5c61f2a5a922510e2306e8349479334
OLGA DEMINA <olga.demina1410@gmail.com>
2023-08-22 00:23:13 +0200

    Add: hero parallax effect

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,8 +1,21 @@
 <script setup>
+const applyParallax = (e) => {
+  document.querySelectorAll(".layer").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+    console.log(speed);
+    if (speed != 0) {
+      console.log('move');
+      const x = (e.pageX * speed) / 10;
+      const y = (e.pageY * speed) / 10;
+      layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    }
+  });
+}
+
 </script>
 
 <template>
-  <section class="jumbotron-bg jumbotron section is-medium">
+  <section class="jumbotron-bg jumbotron section is-medium" @mousemove="applyParallax">
     <div class="container">
       <div class="columns">
         <div class="column is-6 is-flex is-flex-direction-column is-align-items-flex-start">
@@ -15,10 +28,17 @@
           <button class="cta button is-large">Entra nella community</button>
         </div>
         <div class="column is-6">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/Z93qHWoG8VE" title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
+          <div class="parallax-container">
+            <img src="hero/computer.png" alt="" class="layer layer-1" data-speed="0">
+            <img src="hero/sun.png" alt="" class="layer layer-2" data-speed="-0.5">
+            <img src="hero/window.png" alt="" class="layer layer-3" data-speed="-0.2">
+            <img src="hero/man-1.png" alt="" class="layer layer-4" data-speed="0">
+            <img src="hero/man-2.png" alt="" class="layer layer-5" data-speed="0">
+            <img src="hero/woman-1.png" alt="" class="layer layer-6" data-speed="0">
+            <img src="hero/html.png" alt="" class="layer layer-7" data-speed="0.5">
+            <img src="hero/css.png" alt="" class="layer layer-8" data-speed="0.1">
+            <img src="hero/brakets.png" alt="" class="layer layer-9" data-speed="0.2">
+          </div>
         </div>
       </div>
     </div>
@@ -28,5 +48,16 @@
 <style scoped>
 .jumbotron {
   margin-top: 96px;
+}
+
+.parallax-container {
+  position: relative;
+  height: 400px;
+}
+
+.layer {
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
