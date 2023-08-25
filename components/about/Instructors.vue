e4afc5a70e55d75ea1ca445e387e886e685912f9
olga_demina <olga.demina1410@gmail.com>
2023-08-25 18:00:45 +0200

    Add: robots and sitemap

--- components/about/Instructors.vue
+++ components/about/Instructors.vue
@@ -14,14 +14,14 @@ import AppSection from "../AppSection.vue";
         <template v-slot:content>
             <div class="column is-3 is-flex is-flex-direction-column is-align-items-center">
                 <figure class="image is-128x128">
-                    <img class="is-rounded" src="adriano.jpeg" alt="Adriano Grimaldi">
+                    <img class="is-rounded" src="/adriano.jpeg" alt="Adriano Grimaldi">
                 </figure>
                 <p class="mt-4 title is-4">Adriano Grimaldi</p>
                 <p class="subtitle is-5">FullStack Web Developer</p>
             </div>
             <div class="column is-3 is-flex is-flex-direction-column is-align-items-center">
                 <figure class="image is-128x128">
-                    <img class="is-rounded" src="olga.jpeg" alt="Olga Demina">
+                    <img class="is-rounded" src="/olga.jpeg" alt="Olga Demina">
                 </figure>
                 <p class="mt-4 title is-4">Olga Demina</p>
                 <p class="subtitle is-5">FullStack Web Developer</p>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,28 +3,35 @@ import { env } from "process";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['bulma', '~/assets/scss/main.scss'],
+  css: ["~/assets/scss/main.scss", "bulma"],
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' },
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
     head: {
       link: [
         {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-          crossorigin: "anonymous"
-        }
-      ]
-    }
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+          crossorigin: "anonymous",
+        },
+      ],
+    },
   },
   runtimeConfig: {
     public: {
       apiBase: `https://api.airtable.com/v0/${process.env.PUBLIC_API_BASE_ID}`,
-      apiToken: process.env.PUBLIC_API_TOKEN
-    }
+      apiToken: process.env.PUBLIC_API_TOKEN,
+    },
   },
   modules: [
-    '@nuxt/image',
-    '@nuxt/robots',
-  ]
-})
+    "@nuxt/image",
+    "nuxt-simple-sitemap",
+    [
+      "@nuxtjs/robots",
+      {
+        UserAgent: "*",
+        Disallow: "/",
+      },
+    ],
+  ],
+});
